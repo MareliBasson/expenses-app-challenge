43dac5d2fe02b6c0c22cc20650faa5145bb7da04
Mareli Basson <mareli@journeyapps.com>
2020-02-05 18:23:36 +0200

    setup api call for expenses (sandbox)

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,34 @@
-import React, { Component } from "react";
-import PageTemplate from "components/page-template";
-import "./home.css";
+import React, { Component } from 'react'
+import PageTemplate from 'components/page-template'
+import './home.css'
 
 class HomePage extends Component {
-  render() {
-    return (
-      <PageTemplate pageHead="Home Page">
-        <p>Some text goes here</p>
-      </PageTemplate>
-    );
-  }
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			expenses: []
+		}
+	}
+
+	componentDidMount() {
+		fetch('http://localhost:3000/expenses')
+			.then(response => response.json())
+			.then(expenses => this.setState({ expenses: expenses.expenses }))
+			.catch(err => {
+				console.log(err)
+			})
+	}
+
+	render() {
+		console.log(this.state.expenses)
+
+		return (
+			<PageTemplate pageHead="Home Page">
+				<p>Some text goes here</p>
+			</PageTemplate>
+		)
+	}
 }
 
-export default HomePage;
+export default HomePage
