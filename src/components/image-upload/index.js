6c46ed9ec5854c5aafa664330b2504c45f425183
Mareli Basson <mareli@journeyapps.com>
2020-02-09 10:01:19 +0200

    remove firebase installation

--- src/components/image-upload/index.js
+++ src/components/image-upload/index.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react'
-import storage from 'firebase/index.js'
 
 class ImageUpload extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			image: null,
-			url: '',
-			progress: 0
+			image: null
+			// url: '',
+			// progress: 0
 		}
 
+		this.handleChange = this.handleChange.bind(this)
 		this.saveImages = this.saveImages.bind(this)
-	}
-
-	handleChange = e => {
-		if (e.target.files[0]) {
-			this.setState({ image: e.target.files[0] })
-		}
 	}
 
 	// handleUpload = () => {
@@ -46,17 +40,23 @@ class ImageUpload extends Component {
 	// 	)
 	// }
 
+	handleChange = e => {
+		// if (e.target.files[0]) {
+		this.setState({ image: e.target.files[0] })
+		// }
+	}
+
 	saveImages(event) {
 		event.preventDefault()
+
 		console.log('saving image')
 
-		const formData = new FormData()
+		let formData = new FormData()
 		formData.append('receipt', this.state.image)
 
 		fetch(`http://localhost:3000/expenses/${this.props.id}/receipts`, {
 			method: 'POST',
 			headers: {
-				Accept: 'multipart/form-data',
 				'Content-Type': 'multipart/form-data'
 			},
 			body: formData
@@ -82,18 +82,18 @@ class ImageUpload extends Component {
 						<input type="file" onChange={this.handleChange} />
 					</div>
 				</div>
-				<button onClick={this.handleUpload} className="waves-effect waves-light btn">
+				<button onClick={this.saveImages}>Upload</button>
+
+				{/* <button onClick={this.handleUpload} className="waves-effect waves-light btn">
 					Upload <progress value={this.state.progress} max="100" className="progress" />
 				</button>
-				{/* <div className="image-preview">
+				<div className="image-preview">
 					<img
 						src={this.state.url || 'https://via.placeholder.com/400x300'}
 						width="200"
 						alt="Uploaded Images"
 					/>
 				</div> */}
-
-				<button onClick={this.saveImages}>Save Receipts</button>
 			</div>
 		)
 	}
