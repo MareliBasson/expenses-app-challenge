b33e028ebfa308c58581169a64a26c9f5ead5525
Mareli Basson <ambra.lioness@gmail.com>
2020-02-12 19:57:44 +0200

    routine clean up

--- api/routes/expenses.ts
+++ api/routes/expenses.ts
@@ -50,8 +50,6 @@ router.post('/:id', (req, res) => {
 
 	if (expense) {
 		expense.comment = req.body.comment || expense.comment
-		expense.category = req.body.category || expense.category
-		console.log(expense, req.body)
 		res.status(200).send(expense)
 	} else {
 		res.status(404)
